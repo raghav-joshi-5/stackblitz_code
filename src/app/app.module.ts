9bff83519fd86a6c2b5cce9d59ab756c22d9d98c
raghav-joshi-5 <raghav.joshi2017@gmail.com>
2025-01-12 14:32:56 +0530

    chenge1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TdformComponent } from './shared/component/tdform/tdform.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TdformComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/component/tdform/tdform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-tdform',
+  templateUrl: './tdform.component.html',
+  styleUrls: ['./tdform.component.scss']
+})
+export class TdformComponent implements OnInit {
+ 
+  constructor() {}
+  ngOnInit(): void {
+  }
+
+}
